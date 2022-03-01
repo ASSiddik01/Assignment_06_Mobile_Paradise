b616fbaa5fe09c32839ef8928c4d2d2157a0f0de
Shama <assiddik001@gmail.com>
2022-03-01 15:49:39 +0600

    display phone details and main feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,6 @@ const displayPhone = (phones) => {
     phoneContainer.textContent = '';
     // Loop on seach result
     searchResult.forEach(phone => {
-        console.log(phone)
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
@@ -47,8 +46,33 @@ const displayPhone = (phones) => {
 // Load Phone Details
 const loadPhoneDetails = id => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
-    console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(res => displayPhoneDetails(res.data));
+}
+
+// Display Phone Details
+const displayPhoneDetails = phone => {
+    console.log(phone.mainFeatures);
+    const phoneDetailes = document.getElementById('phone_detiles');
+    console.log(phoneDetailes);
+    phoneDetailes.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card')
+    div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${phone.name}</h5>
+            <p class="card-text"><span class="text-primary">Release Date: </span>${phone.releaseDate} </p>
+            <p class="card-text text-primary">Main Feature</p>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><span class="text-primary">Chipset: </span>${phone.mainFeatures.chipSet}</li>
+                <li class="list-group-item"><span class="text-primary">Display Size: </span>${phone.mainFeatures.displaySize}</li>
+                <li class="list-group-item"><span class="text-primary">Memory: </span>${phone.mainFeatures.memory}</li>
+                <li class="list-group-item"><span class="text-primary">Storage: </span>${phone.mainFeatures.storage}</li>
+            </ul>
+
+        </div>
+        `;
+    phoneDetailes.appendChild(div);
 }
