ffda0a16c82c3f041679c3f9c973df2e5280e16c
Shama <assiddik001@gmail.com>
2022-03-01 14:44:43 +0600

    Display phone

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ const phoneSearch = () => {
     const searchText = inputText.value;
     loadData(searchText);
     inputText.value = '';
-}
+};
 
 // Load API Information
 const loadData = (searchText) => {
@@ -14,11 +14,33 @@ const loadData = (searchText) => {
     fetch(url)
         .then(res => res.json())
         .then(res => displayPhone(res.data));
-}
+};
 
 // Display Phone
 const displayPhone = (phones) => {
-    const expectPhones = phones.slice(0, 20);
-    // console.log(expectPhones);
-    expectPhones.forEach(phone => console.log(phone));
-}
+    // Get max 20 phones
+    const searchResult = phones.slice(0, 20);
+    // Get Container form UI
+    const phoneContainer = document.getElementById('display_phone');
+    // Clear Container form UI
+    phoneContainer.textContent = '';
+    // Loop on seach result
+    searchResult.forEach(phone => {
+        console.log(phone)
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML = `
+            <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">Brand: <span class="text-primary">${phone.brand}</span> </p>
+                    <button type="button" class="btn btn-outline-dark">Show Details</button>
+                </div>
+            </div>
+        `;
+        // Append Phone div
+        phoneContainer.appendChild(div);
+
+    });
+};
