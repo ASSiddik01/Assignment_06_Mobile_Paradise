460963abd9c945dbe010b5d2ea66da2a3e6aef0e
Shama <assiddik001@gmail.com>
2022-03-01 22:09:28 +0600

    Final Check

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,39 @@
 // Search Phone by input
 const phoneSearch = () => {
-  // Spinner and error message handle
+  // Error message handle
   document.getElementById('result_not_found').style.display = 'none';
   document.getElementById('empty_search').style.display = 'none';
+
+  // Get Container and empty form UI
+  const phoneContainer = document.getElementById('display_phone');
+  phoneContainer.textContent = '';
+  const phoneDetailes = document.getElementById('phone_detiles');
+  phoneDetailes.textContent = '';
+  
+  // Spinner Handle
   toggleSpinner('block');
 
+  // Get Input value and case convert
   const inputText = document.getElementById('phone_name');
   const searchText = inputText.value.toLowerCase();
+
   // Empty search handle
   if (searchText == '') {
     document.getElementById('empty_search').style.display = 'block';
     toggleSpinner('none');
     return;
   }
+
   loadPhone(searchText);
   inputText.value = '';
 };
 
-
+// Spinner Handle
 const toggleSpinner = displayStyle => {
   document.getElementById('spinner').style.display = displayStyle;
 }
 
-// Load API Information
+// Load Phone Information
 const loadPhone = (searchText) => {
   let url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   fetch(url)
@@ -32,24 +43,25 @@ const loadPhone = (searchText) => {
 
 // Display Phone
 const displayPhone = (phones) => {
+
   // Spinner and error message handle
   if (phones.length == 0) {
     toggleSpinner('none');
     document.getElementById('result_not_found').style.display = 'block';
   }
+
   // Get max 20 phones
   const searchResult = phones.slice(0, 20);
-  // Get Container and empty form UI
+
+  // Get Container
   const phoneContainer = document.getElementById('display_phone');
-  phoneContainer.textContent = '';
-  const phoneDetailes = document.getElementById('phone_detiles');
-  phoneDetailes.textContent = '';
-  // Loop on seach result
+
+  // Loop on search result
   searchResult.forEach(phone => {
     const div = document.createElement('div');
     div.classList.add('col')
     div.innerHTML = `
-            <div class="card border-0 shadow-lg h-100 text-center">
+            <div class="card border-0 shadow h-100 text-center">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">${phone.phone_name}</h5>
@@ -80,7 +92,7 @@ const loadPhoneDetails = id => {
 const displayPhoneDetails = phone => {
   const features = phone.mainFeatures;
 
-  // Other info undefind handle
+  // Other info undefiend handle
   let WLAN = phone?.others?.WLAN;
   let Bluetooth = phone?.others?.Bluetooth;
   let GPS = phone?.others?.GPS;
@@ -104,12 +116,13 @@ const displayPhoneDetails = phone => {
     date = notFound;
   }
 
+  // Show more details
   const phoneDetailes = document.getElementById('phone_detiles');
   phoneDetailes.textContent = '';
   const div = document.createElement('div');
   div.classList.add()
   div.innerHTML = `
-    <div class="card mb-3 mx-auto p-3" style="max-width: 540px;">
+    <div class="card shadow-lg mb-3 mx-auto p-3" style="max-width: 540px;">
             <div class="row g-0">
               <div class="col-lg-4">
                 <img src="${phone.image}" class="img-fluid rounded-start" alt="...">
